2ef805dfe500608dd68b37683d10cc748200ab1b
pablo-lucas <pablo.lucv@gmail.com>
2022-07-16 22:18:32 -0500

    Added App class component and constructor

--- src/App.js
+++ src/App.js
@@ -1,2 +1,22 @@
 import React from "react";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const App= () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    )
-    return(
-        <div>Latitude: </div>
-    )
-}
 
 root.render(
   <React.StrictMode>
